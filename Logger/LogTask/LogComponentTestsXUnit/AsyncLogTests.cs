b81ed45fa2fe1aeb7bb4043153965ce69b650b27
Serzas P <slipik@inbox.ru>
2020-03-14 19:04:56 +0200

    Test checking stopping log's writing with no flush or with flush implemented; file picking logic changed to pick up the needed one.

--- Logger/LogTask/LogComponentTestsXUnit/AsyncLogTests.cs
+++ Logger/LogTask/LogComponentTestsXUnit/AsyncLogTests.cs
@@ -14,51 +14,52 @@ namespace LogComponentTestsXUnit
     public class AsyncLogTests
     {
         private const string logDir = @"C:\LogTest\";
-        private const string demoText = "TestWrite to log";
+        private string demoText = DateTime.Now.ToLongTimeString();
 
         [Fact]
-        public void WriteToLogFile()
+        public async Task WriteToLogFile()
         {
+            var dateProviderMock = new Mock<IDateTimeProvider>();
+            var dateTime = new DateTime(2020, 3, 11, 18, 55, 00);
+            dateProviderMock.SetupGet(x => x.DateTimeNow).Returns(dateTime);
+
+            DateTimeProvider.Current = dateProviderMock.Object;
+
             var log = new AsyncLog();
 
-            log.Write(demoText); 
+            log.Write(demoText);
+            Thread.Sleep(50);
             log.StopWithFlush();
 
             Assert.True(Directory.Exists(logDir));
 
             var files = Directory.GetFiles(logDir);
-            var textFound = false;
+            var dateFormatted = dateTime.ToString("yyyyMMdd HHmmss fff");
 
             foreach (var file in files)
             {
-                using (StreamReader streamReader = File.OpenText(file))
+                var fileInfo = new FileInfo(file);
+
+                if (fileInfo.Name.Contains(dateFormatted))
                 {
-                    streamReader.ReadLine();
-                    var readline = streamReader.ReadLine();
-                    if (readline.Contains(demoText))
+                    using (StreamReader streamReader = File.OpenText(file))
                     {
-                        textFound = true;
-                        break;
+                        var fileContent = await streamReader.ReadToEndAsync();
+                        Assert.Contains(fileContent, demoText);
                     }
                 }
             }
-
-            Assert.True(textFound);
         }
 
         [Fact]
         public void NewFilesCreatedAfterMidnight()
         {
-            var now = DateTimeProvider.Current.DateTimeNow;
-
             var timeMock = new Mock<IDateTimeProvider>();
-
             timeMock.SetupGet(x => x.DateTimeNow).Returns(new DateTime(2020, 3, 12, 11, 59, 00));
 
             DateTimeProvider.Current = timeMock.Object;
 
             var log = new AsyncLog();
-
             log.Write(demoText);
             Thread.Sleep(50);
 
@@ -75,6 +76,73 @@ namespace LogComponentTestsXUnit
             int numberOfFilesAfterMidnight = Directory.GetFiles(logDir).Count();
 
             Assert.True(numberOfFilesBeforeMidnight < numberOfFilesAfterMidnight);
+        }
+
+        [Fact]
+        public async Task StopWritingWithoutFlush()
+        {
+            var dateTimeProviderMock = new Mock<IDateTimeProvider>();
+            var dateTime = new DateTime(2020, 3, 14, 12, 30, 00);
+            dateTimeProviderMock.SetupGet(x => x.DateTimeNow).Returns(dateTime);
+
+            DateTimeProvider.Current = dateTimeProviderMock.Object;
+
+            var log = new AsyncLog();
+            log.Write(demoText);
+            Thread.Sleep(50);
+            log.StopWithoutFlush();
+
+            Assert.True(Directory.Exists(logDir));
+            var files = Directory.GetFiles(logDir);
+
+            var formattedDate = dateTime.ToString("yyyyMMdd HHmmss fff");
+
+            foreach (var file in files)
+            {
+                var fileInfo = new FileInfo(file);
+                if (fileInfo.Name.Contains(formattedDate))
+                {
+                    using (var streamReader = File.OpenText(file))
+                    {
+                        var content = await streamReader.ReadToEndAsync();
+                        Assert.DoesNotContain(demoText, content);
+                    }
+                    break;
+                }
+            }
+        }
+
+        [Fact]
+        public async Task StopWritingWithFlush()
+        {
+            var dateTimeProviderMock = new Mock<IDateTimeProvider>();
+            var dateTime = new DateTime(2020, 3, 14, 12, 45, 00);
+            dateTimeProviderMock.SetupGet(x => x.DateTimeNow).Returns(dateTime);
+
+            DateTimeProvider.Current = dateTimeProviderMock.Object;
+
+            var log = new AsyncLog();
+            log.Write(demoText);
+            Thread.Sleep(50);
+            log.StopWithFlush();
+
+            Assert.True(Directory.Exists(logDir));
+
+            var files = Directory.GetFiles(logDir);
+            var formattedDate = dateTime.ToString("yyyyMMdd HHmmss fff");
+            foreach (var file in files)
+            {
+                var fileInfo = new FileInfo(file);
+                if (fileInfo.Name.Contains(formattedDate))
+                {
+                    using (StreamReader fileReader = File.OpenText(file))
+                    {
+                        var fileContent = await fileReader.ReadToEndAsync();
+                        Assert.Contains(fileContent, demoText);
+                    }
+                    break;
+                }
+            }
         }
     }
 }
