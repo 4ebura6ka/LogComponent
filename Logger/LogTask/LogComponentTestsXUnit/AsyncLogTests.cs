3920906001ab2810291396a90188d1d7f9b24b20
Serzas P <slipik@inbox.ru>
2020-03-16 01:09:20 +0200

    Added logs cleaning before running the tests, improved testing cases for without or with flush.

--- Logger/LogTask/LogComponentTestsXUnit/AsyncLogTests.cs
+++ Logger/LogTask/LogComponentTestsXUnit/AsyncLogTests.cs
@@ -14,36 +14,47 @@
         private const string LogDir = @"C:\LogTest\";
         private readonly string _demoText = DateTime.Now.ToLongTimeString();
 
+        public AsyncLogTests()
+        {
+            if (Directory.Exists(LogDir))
+            {
+                var files = Directory.GetFiles((LogDir));
+
+                foreach (var file in files)
+                { 
+                    File.Delete(file);
+                }
+            }
+        }
+
         [Fact]
         public async Task WriteToLogFile()
         {
             var dateProviderMock = new Mock<IDateTimeProvider>();
             var dateTime = new DateTime(2020, 3, 11, 18, 55, 00);
             dateProviderMock.SetupGet(x => x.DateTimeNow).Returns(dateTime);
-
             DateTimeProvider.Current = dateProviderMock.Object;
 
             var log = new AsyncLog();
-
             log.Write(_demoText);
             Thread.Sleep(50);
             log.StopWithFlush();
 
             Assert.True(Directory.Exists(LogDir));
-
             var files = Directory.GetFiles(LogDir);
-            var dateFormatted = dateTime.ToString("yyyyMMdd HHmmss fff");
+
+            var dateTimeInLogfileName = dateTime.ToString("yyyyMMdd HHmmss fff");
 
             foreach (var file in files)
             {
                 var fileInfo = new FileInfo(file);
 
-                if (fileInfo.Name.Contains(dateFormatted))
+                if (fileInfo.Name.Contains(dateTimeInLogfileName))
                 {
-                    using (StreamReader streamReader = File.OpenText(file))
+                    using (var streamReader = File.OpenText(file))
                     {
                         var fileContent = await streamReader.ReadToEndAsync();
-                        Assert.Contains(fileContent, _demoText);
+                        Assert.True(fileContent.Contains(_demoText));
                     }
                 }
             }
@@ -54,23 +65,20 @@
         {
             var timeMock = new Mock<IDateTimeProvider>();
             timeMock.SetupGet(x => x.DateTimeNow).Returns(new DateTime(2020, 3, 12, 11, 59, 00));
-
             DateTimeProvider.Current = timeMock.Object;
 
             var log = new AsyncLog();
             log.Write(_demoText);
-            Thread.Sleep(70);
+            Thread.Sleep(50);
 
             Assert.True(Directory.Exists(LogDir));
 
-            var files = Directory.GetFiles(LogDir);
-
-            int numberOfFilesBeforeMidnight = files.Count();
+            int numberOfFilesBeforeMidnight = Directory.GetFiles(LogDir).Count();
 
             timeMock.SetupGet(x => x.DateTimeNow).Returns(new DateTime(2020, 3, 13, 00, 01, 00));
-            log.Write(_demoText);
 
-            Thread.Sleep(70);
+            log.Write(_demoText);
+            Thread.Sleep(50);
 
             int numberOfFilesAfterMidnight = Directory.GetFiles(LogDir).Count();
 
@@ -83,28 +91,31 @@
             var dateTimeProviderMock = new Mock<IDateTimeProvider>();
             var dateTime = new DateTime(2020, 3, 14, 12, 30, 00);
             dateTimeProviderMock.SetupGet(x => x.DateTimeNow).Returns(dateTime);
-
             DateTimeProvider.Current = dateTimeProviderMock.Object;
 
             var log = new AsyncLog();
             log.Write(_demoText);
-            Thread.Sleep(50);
+            for (var i = 0; i <= 100; i++)
+            {
+                log.Write($"{_demoText}{i}");
+                Thread.Sleep(50);
+            }
             log.StopWithoutFlush();
 
             Assert.True(Directory.Exists(LogDir));
             var files = Directory.GetFiles(LogDir);
 
-            var formattedDate = dateTime.ToString("yyyyMMdd HHmmss fff");
+            var dateTimeInLogfileName = dateTime.ToString("yyyyMMdd HHmmss fff");
 
             foreach (var file in files)
             {
                 var fileInfo = new FileInfo(file);
-                if (fileInfo.Name.Contains(formattedDate))
+                if (fileInfo.Name.Contains(dateTimeInLogfileName))
                 {
                     using (var streamReader = File.OpenText(file))
                     {
                         var content = await streamReader.ReadToEndAsync();
-                        Assert.DoesNotContain(_demoText, content);
+                        Assert.False(_demoText.Contains(content));
                     }
                     break;
                 }
@@ -117,27 +128,30 @@
             var dateTimeProviderMock = new Mock<IDateTimeProvider>();
             var dateTime = new DateTime(2020, 3, 14, 12, 45, 00);
             dateTimeProviderMock.SetupGet(x => x.DateTimeNow).Returns(dateTime);
-
             DateTimeProvider.Current = dateTimeProviderMock.Object;
 
             var log = new AsyncLog();
-            log.Write(_demoText);
-            Thread.Sleep(50);
+            for (var i = 0; i <= 100; i++)
+            {
+                log.Write($"{_demoText}{i}");
+                Thread.Sleep(50);
+            }
+
             log.StopWithFlush();
 
             Assert.True(Directory.Exists(LogDir));
 
             var files = Directory.GetFiles(LogDir);
-            var formattedDate = dateTime.ToString("yyyyMMdd HHmmss fff");
+            var dateTimeInLogfileName = dateTime.ToString("yyyyMMdd HHmmss fff");
             foreach (var file in files)
             {
                 var fileInfo = new FileInfo(file);
-                if (fileInfo.Name.Contains(formattedDate))
+                if (fileInfo.Name.Contains(dateTimeInLogfileName))
                 {
                     using (StreamReader fileReader = File.OpenText(file))
                     {
                         var fileContent = await fileReader.ReadToEndAsync();
-                        Assert.Contains(fileContent, _demoText);
+                        Assert.True(fileContent.Contains($"{_demoText}101"));
                     }
                     break;
                 }
