bb41df9269736bd49c8411b440c0930a575e376f
Serzas P <slipik@inbox.ru>
2020-03-31 19:31:18 +0300

    var placed instead explicit typing, renamed loggers.

--- Logger/LogTask/Application/Program.cs
+++ Logger/LogTask/Application/Program.cs
@@ -5,25 +5,25 @@ namespace LogUsers
     using System;
     using LogTest;
 
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         { 
-            ILog  logger = new AsyncLog(new LogsStorage());
-            for (int i = 0; i < 15; i++)
+            var flushLogger = new AsyncLog(new LogsStorage());
+            for (var i = 0; i < 15; i++)
             {
-                logger.Write($"Number with Flush: {i}");
+                flushLogger.Write($"Number with Flush: {i}");
                 Task.Delay(10).Wait();
             }
-            logger.StopWithFlush();
+            flushLogger.StopWithFlush();
 
-            ILog logger2 = new AsyncLog(new LogsStorage());
-            for (int i = 50; i > 0; i--)
+            var noFlushLogger = new AsyncLog(new LogsStorage());
+            for (var i = 50; i > 0; i--)
             {
-                logger2.Write($"Number with No flush: {i}");
+                noFlushLogger.Write($"Number with No flush: {i}");
                 Task.Delay(5).Wait();
             }
-            logger2.StopWithoutFlush();
+            noFlushLogger.StopWithoutFlush();
 
             Console.ReadKey();
         }
